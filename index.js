7d404c627facbff341161330941a46685f55c853
Bernard Legaspi <blegaspi26@gmail.com>
2022-09-22 11:01:53 -0400

    commit tree a643952e156846a4734dd4731328521effe88069

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+    });
 }
+
+
+/*
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+  }); 
+
+  
+ */
